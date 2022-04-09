df512b16727e9b57e32cb1d2bbf88342ded71a49
Brandon Quinn <brandonjquinn@gmail.com>
2022-04-09 13:15:44 -0700

    Parallax works in production but looks janks in editor

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -9,25 +9,26 @@ public class Follower : MonoBehaviour
     public bool followY = true;
     public float scale = 1;
 
-    private Vector3 targetLastPosition;
+    private Vector3 startPosition;
+    private Vector3 targetStartPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetLastPosition = followTarget.transform.position;
+        startPosition = transform.position;
+        targetStartPosition = followTarget.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 shift = followTarget.position - targetLastPosition;
+        Vector3 shift = followTarget.position - targetStartPosition;
 
         shift *= scale;
 
         float x = followX ? shift.x : 0;
         float y = followY ? shift.y : 0;
 
-        transform.position += new Vector3(x, y, 0);
-        targetLastPosition = followTarget.position;
+        transform.position = startPosition + new Vector3(x, y, 0);
     }
 }
